d6ee5eec0bac765000efb9412c8885babac3b982
Parviz-Alakbarov <parvizja@code.edu.az>
2023-02-11 19:29:01 +0400

    ✅Message sending feature added

--- users/urls.py
+++ users/urls.py
@@ -9,10 +9,14 @@ urlpatterns = [
     path('edit-account/', views.editAccount, name='edit-account'),
 
     path('create-skill/', views.createSkill, name='create-skill'),
-    path('update-skill/<str:pk>', views.updateSkill, name='update-skill'),
-    path('delete-skill/<str:pk>', views.deleteSkill, name='delete-skill'),
+    path('update-skill/<str:pk>/', views.updateSkill, name='update-skill'),
+    path('delete-skill/<str:pk>/', views.deleteSkill, name='delete-skill'),
+
+    path('inbox/', views.inbox, name='inbox'),
+    path('message/<str:pk>/', views.viewMessage, name='message'),
+    path('create-message/<str:pk>/', views.createMessage, name='create-message'),
 
     path('', views.profiles, name='profiles'),
     path('users/', views.profiles, name='profiles'),
-    path('profile/<str:pk>', views.userProfile, name='user-profile')
+    path('profile/<str:pk>/', views.userProfile, name='user-profile')
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from .models import Profile
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .models import Profile, Message
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from .utils import searchProfiles, paginateProfiles
 
 
@@ -18,6 +18,7 @@ def profiles(request):
         'custom_range': custom_range,
         'before_last_page': paginator_num_pages - 1
     })
+
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
@@ -33,7 +34,7 @@ def loginPage(request):
         return redirect('profiles')
 
     if request.method == 'POST':
-        username = request.POST['username']
+        username = request.POST['username'].lower()
         password = request.POST['password']
         try:
             user = User.objects.get(username=username)
@@ -43,7 +44,7 @@ def loginPage(request):
 
         if user is not None:
             login(request, user)
-            return redirect('profiles')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
         else:
             messages.error(request, 'Username or Password is incorrect')
 
@@ -142,3 +143,52 @@ def deleteSkill(request, pk):
         messages.success(request, 'Skill Deleted!')
         return redirect('account')
     return render(request, 'delete_template.html', {'object': skill})
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    unreadCount = messageRequests.filter(is_read=False).count()
+
+    return render(request, 'users/inbox.html', {'messageRequests': messageRequests, 'unreadCount': unreadCount})
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if not message.is_read:
+        message.is_read = True
+        message.save()
+    return render(request, 'users/message.html', {'message': message})
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    if recipient == request.user.profile:
+        messages.error(request, 'You can\'t send a message to yourself!')
+        return redirect('profiles')
+
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+
+            message.save()
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+    return render(request, 'users/message_form.html', {'recipient': recipient, 'form': form})
